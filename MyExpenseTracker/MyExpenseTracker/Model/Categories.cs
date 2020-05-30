90bac0eecb007147240f7ce180882dd8704756e8
srkulkarni20 <64452229+srkulkarni20@users.noreply.github.com>
2020-05-29 21:24:26 -0700

    Add project files.

---
+++ MyExpenseTracker/MyExpenseTracker/Model/Categories.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyExpenseTracker.Model
+{
+    public class Categories
+    {
+        public string Name { get; set; }
+        public double Spent { get; set; }
+        public double Balance { get; set; }
+        public double Budget { get; set; }
+        public string IconSource { get; set; }
+
+        public List<Expense> expenses;
+
+
+        public Categories(string name, double spent, double balance, double budget, string Iconsource)
+        {
+            this.Name = name;
+            this.Spent = spent;
+            this.Balance = balance;
+            this.Budget = budget;
+            this.IconSource = Iconsource;
+        }
+
+    }
+}
